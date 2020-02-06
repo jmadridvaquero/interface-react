b52a55ea551181466493031e4da2517338d58b40
Younes <mia.san.younes@gmail.com>
2020-02-05 21:11:29 -0500

    services data works

--- src/Service.js
+++ src/Service.js
@@ -2,17 +2,17 @@ import React, {useState, useEffect} from "react"
 
 
 function Service() {
-    const [state, setState] = useState()
+    const [state, setState] = useState({loading: true})
     
     useEffect(() => {
         fetch("https://api.airtable.com/v0/appmGkj4JSgjTAE3K/Services?api_key=keyymnrx7vCKyPaSr")
             .then((resp) => resp.json())
-            .then(data => { setState({ services: data.records })});
+            .then(data => { setState({ services: data.records, loading: false })});
         }, [])
         console.log(state, "state")
     
 
-    return( <p>Service</p> )
+    return( <p>{state.loading ? null : state.services[0].fields.Name}</p> )
 
 }
 
